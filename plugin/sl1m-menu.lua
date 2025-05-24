1166ae6c9182e8a7410d69cdfe56be5343664e20
sL1m <boggsja83@gmail.com>
2025-05-24 03:43:51 -0400

    .

--- plugin/sl1m-menu.lua
+++ plugin/sl1m-menu.lua
@@ -1,10 +1,8 @@
 
 return
 {
-    --dir = vim.fn.stdpath("config") .. "/lua/sl1m-menu",
     config = function()
 	require("sl1m-menu").setup()
-	--vim.keymap.set("n", "<leader>T",sl1m.sl1mfunc, { noremap = false, desc = "DeZ NO0tS"})
     end,
 
 }
